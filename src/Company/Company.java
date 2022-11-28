2cd6beaaf4761071e3e61d6861b1e4f24e3172fb
Dima <dmitriy.matveev89@inbox.ru>
2022-11-28 16:42:02 +0300

    Initial commit

---
+++ src/Company/Company.java
@@ -0,0 +1,38 @@
+package Company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+    /* найм одного сотрудника */
+    private void hire() {
+
+    }
+
+    /* найм списка сотрудников */
+    private void hireAll() {
+
+    }
+
+    /* увольнение сотрудника */
+    private void fire() {
+
+    }
+
+    /* получение значения дохода компании */
+    private void getIncome() {
+
+    }
+
+    /* Они должны содержать сотрудников, отсортированных по убыванию и возрастанию заработной платы */
+    private List<Employee> getTopSalaryStaff(int count) {
+
+        return new ArrayList<>();
+    }
+
+    /* Они должны содержать сотрудников, отсортированных по убыванию и возрастанию заработной платы */
+    private List<Employee> getLowestSalaryStaff(int count) {
+
+        return new ArrayList<>();
+    }
+}
